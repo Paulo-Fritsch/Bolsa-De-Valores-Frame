394b5bb4d81c4b3931055454b98771cecb8b98fc
Paulo Cezar Fritsch Júnior <paulo.fritsch2001@gmail.com>
2021-08-09 11:58:59 -0300

    Tratando as datas no controller

--- client/js/app/controllers/NegociacaoControle.js
+++ client/js/app/controllers/NegociacaoControle.js
@@ -1,19 +1,35 @@
 class NegociacaoControle {
-    constructor(){
-        let $ = document.querySelector.bind (document); // criando uma estilização parecida com jquery
-        this._inputData = $("#data");
-        this._inputQuantidade = $("#quantidade");
-        this._inputValor = $("#valor");
+
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
 
     }
 
     adiciona(event) {
 
         event.preventDefault();
-    
-        let data = new Date(this._inputData.value.replace(/-/g, ','));
-        console.log(data);
+
+        let data = new Date(...this._inputData
+            .value.split('-')
+            .map(function(item, indice) {
+                return item - indice % 2;
+            })); 
+        //Em tese, o conjunto em computação começa em 0, sendo assim
+        // o valor 0 recebe janeiro e assim por diante, mas ñ é isso que 
+        // queremos, então fazemos um map da função, e retornamos esse item 
+        // com seu indice subtraindo, para sempre termos uma subtração em 
+        // apenas nos meses...
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
     }
-
 }
-
