d8bd8e1f554cd8a68a16c148815fb7be964a1874
Paulo Cezar Fritsch Júnior <paulo.fritsch2001@gmail.com>
2021-08-09 11:40:33 -0300

    Atualizando o models

---
+++ client/js/app/controllers/NegociacaoControle.js
@@ -0,0 +1,19 @@
+class NegociacaoControle {
+    constructor(){
+        let $ = document.querySelector.bind (document); // criando uma estilização parecida com jquery
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+
+    }
+
+    adiciona(event) {
+
+        event.preventDefault();
+    
+        let data = new Date(this._inputData.value.replace(/-/g, ','));
+        console.log(data);
+    }
+
+}
+
--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -2,24 +2,29 @@ class Negociacao {
 
     constructor(data, quantidade, valor) {
 
-        this._data = data;
+        this._data = new Date (data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze (this);
 
     }
 
-    getVolume(){
+    // Encapsulando na convenção de colocar _ (underline), como var privadas
+    get volume(){
         return this._quantidade * this._valor;  
     }
-    getData(){
-        return this._data;
-    }
+    get data(){
+        return new Date(this._data.getTime());
+    } // Criando uma data para ser imutável, programação defensiva
 
-    getQuantidade(){
+    get quantidade(){
         return this._quantidade;
     }
 
-    getValor (){
+    get valor(){
         return this._valor;
-    }
+    } 
+
+    //getter
+
 }
