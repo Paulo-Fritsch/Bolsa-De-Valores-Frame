857c44e862532d2c55f771649afc7861ba041a50
Paulo Cezar Fritsch Júnior <paulo.fritsch2001@gmail.com>
2021-08-10 19:09:45 -0300

    volume total de negociações

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -19,18 +19,30 @@ class NegociacoesView {
                     </thead>
 
                     <tbody>
-                        ${model.negociacoes.map(n => {
+                        ${model.negociacoes.map(n => `
+                
+                          <tr>
+                            <td>${DateHelper.dataTexto(n.data)}</td>
+                            <td>${n.quantidade}</td>
+                            <td>${n.valor}</td>
+                            <td>${n.volume}</td>
+                          </tr>
+                
+                      `).join('')}
+                </tbody>
 
-                            return `
-                            <tr>
-                                <td>${DateHelper.dataTexto(n.data)}</td>
-                                <td>${n.quantidade}</td>
-                                <td>${n.valor}</td>
-                                <td>${n.volume}</td>
-                            </tr>
-                            `
-                            }).join('')}
-                    </tbody>
+                <tfoot>
+                    <td colspan="3"></td>
+                    <td>${
+                        (function() {
+                
+                            let total = 0;
+                            model.negociacoes.forEach(n => total+= n.volume);
+                            return total;
+                    })()
+                    }
+                    </td>
+                </tfoot>
             </table>
         `;
     }
